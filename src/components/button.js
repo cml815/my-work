8ac17df78195b2d05c6bf156f2b58ea06e77e542
Caroline Leopold <caroline.leopold@gmail.com>
2021-12-23 14:15:44 -0500

    start Button component

---
+++ src/components/button.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import styles from 'button.module.css';
+
+const Button = ({children, onClick, btnColor='purple', labelColor, disabled, type, style, ...props}) => {
+  
+}
