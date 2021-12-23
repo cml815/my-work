722ee7918362ccc3528a8986bb2e60eb8cf55e8f
Caroline Leopold <caroline.leopold@gmail.com>
2021-12-23 14:27:51 -0500

    Navbar, button start

--- src/components/button.js
+++ src/components/button.js
@@ -2,5 +2,13 @@ import React from 'react';
 import styles from 'button.module.css';
 
 const Button = ({children, onClick, btnColor='purple', labelColor, disabled, type, style, ...props}) => {
-  
+  return (
+    <button className={styles.btn}>
+      {children || 'label'}
+    </button>
+  )
+
 }
+
+export default Button
+
