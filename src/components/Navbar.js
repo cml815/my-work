019e8dddccf81912869d3c3fb78c5c7f1ce255cd
Caroline Leopold <caroline.leopold@gmail.com>
2021-12-23 23:34:44 -0500

    add Link to Navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,10 +3,13 @@ import {  Link } from "react-router-dom";
 
 const Navbar = () => {
   return (
-    <div>
-    <ul>
+    <div className="container">
+    <ul className="menu">
        <li>
           <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="#">About</Link>
         </li>
     </ul>
     </div>
